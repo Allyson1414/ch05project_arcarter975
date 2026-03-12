c797bba202b3d93d7cdd1955eae5115ba1532ac9
Allyson Carter <arcarter975@mail.cfcc.edu>
2026-03-12 10:28:37 -0400

    2nd update

---
+++ scripts/form_validation.js
@@ -0,0 +1,10 @@
+/* Javascript for index.html */
+
+checkMissing(
+    displayMe(document.getElementById('missing-count').value)
+);
+
+function validate_form(inform){
+    let warnArr = document.getElementById('missing-count');
+    let ndx;
+}
